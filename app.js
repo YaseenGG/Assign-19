208ba68c81e8e20358147e864ce4706a100c85ed
M Yaseen <yaseenghouri2@gmail.com>
2024-03-04 14:15:03 +0500

    Assign 19

---
+++ app.js
@@ -0,0 +1,124 @@
+// var name1 = "me"; name2 = "he", name3 = "she", name4 = "we";
+// var names = ["me","he","she","we"];
+// names.unshift("zebra");
+// names.shift();
+// names.splice(2,2,"hana","hena")
+// names.push ("cat","dog","man")
+// var names1 = names.slice (2,3)
+// console.log(names);
+
+// for (var i = 20; i >=  0 ; i=i-4){
+//     document.write(i + "<br>")
+// }
+
+// Q1
+Array = []
+
+// Q2
+Array = []
+
+// Q3
+
+Array = ["hel","wow"]
+
+// Q4
+
+Array = [4,1]
+
+// Q5
+
+Array = [true,false]
+
+// Q6
+
+Array = ["hi",4,"cat"]
+
+// Q7
+
+degrees = ["SSC" +"<br>"+ "HSC" +"<br>"+ "BCS"+"<br>"+"BS"+"<br>"+ "BCOM"+"<br>"+ "MS"+"<br>"+ "M. Phil."+"<br>"+ "PhD"+"<br>"]
+document.write(degrees)
+// Q8
+
+names = ["Michael","John","Tony"]
+marks = [320,230,480]
+document.write("Score of Michael is 320. Percentage: 64%<br>Score of John is 230. Percentage: 46%<br>Score of Tony is 480. Percentage: 96%<br>")
+
+
+// Q9
+
+colors = ["red","blue","green","yellow","white"]
+colors.unshift(prompt("enter a start color.."))
+document.write(colors)
+colors.push(prompt("enter a end color"))
+document.write(colors)
+
+
+// PDF Chapter 14
+
+// Q1
+
+var arr= []
+
+// Q2
+
+arr = ["hello"]
+
+// Q3
+
+var alphabet = ["h","i","j","k"]
+var letterJ = alphabet[2];
+alert(letterJ)
+
+// Q4
+
+var alphabet1 = ["h","i","j","k", "l","m","n","o"];
+console.log(alphabet1);
+
+// Q5
+
+var ar = ["he","ha"], ha=ar[1];
+alert (ha)
+
+// Chapter 15
+
+// Q1
+
+var array = ["hell"];
+array.push ("hey")
+alert(myArray[myArray.length - 1]);
+
+// Q2
+
+var Alphabet=["h","i","j","k"]
+Alphabet.pop()
+
+// Q3
+
+var Alphabet=["h","i","j","k"]
+Alphabet.push(44)
+
+// Chapter 16
+
+// Q1
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+sizes.shift()
+
+// Q2
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+sizes.unshift("V","u","y");
+
+// Q3
+var sizes1 = ["S"];
+sizes1.unshift(0)
+alert(sizes1[0])
+
+// Q4
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+var index = sizes.indexOf("M");
+sizes.splice(index + 1, 0, "L");
+
+// Q5
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+var regSizes = sizes.slice(0, 3);
+
+// Q6
